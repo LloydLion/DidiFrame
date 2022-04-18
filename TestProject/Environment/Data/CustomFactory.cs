cf4ecffc6cc3badbbdb335f5cdeaed673197871e
LloydLion <lloydlion2211@gmail.com>
2022-04-18 18:23:38 +0300

    Data test enviroment tests

---
+++ TestProject/Environment/Data/CustomFactory.cs
@@ -0,0 +1,22 @@
+﻿using CGZBot3.Data;
+using System;
+
+namespace TestProject.Environment.Data
+{
+	internal class CustomFactory<TModel> : IModelFactory<TModel> where TModel : class
+	{
+		private readonly Func<TModel> factory;
+
+
+		public CustomFactory(Func<TModel> factory)
+		{
+			this.factory = factory;
+		}
+
+
+		public TModel CreateDefault()
+		{
+			return factory();
+		}
+	}
+}
---
+++ TestProject/Environment/Data/CustomFactoryProvider.cs
@@ -0,0 +1,21 @@
+﻿using CGZBot3.Data;
+
+namespace TestProject.Environment.Data
+{
+	internal class CustomFactoryProvider<TFactory> : IModelFactoryProvider where TFactory : class
+	{
+		private readonly IModelFactory<TFactory> factory;
+
+
+		public CustomFactoryProvider(IModelFactory<TFactory> factory)
+		{
+			this.factory = factory;
+		}
+
+
+		public IModelFactory<TModel> GetFactory<TModel>() where TModel : class
+		{
+			return (IModelFactory<TModel>)factory;
+		}
+	}
+}
