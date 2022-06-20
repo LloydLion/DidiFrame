f1175631ed9f35719ad4d548fab3d695f783fa91
LloydLion <lloydlion2211@gmail.com>
2022-06-20 10:15:53 +0300

    Embeded lifetimes

---
+++ DidiFrame/Data/Lifetime/EmbededLifetimeStateUpdater.cs
@@ -0,0 +1,35 @@
+﻿namespace DidiFrame.Data.Lifetime
+{
+	public class EmbededLifetimeStateUpdater<TBase, TTarget> : ILifetimeStateUpdater<TTarget> where TBase : class, ILifetimeBase where TTarget : class, ILifetimeBase
+	{
+		private readonly Action<TTarget> applyDelegate;
+		private readonly ILifetime<TBase> baseLifetime;
+		private readonly ILifetimeStateUpdater<TBase>? baseUpdater;
+
+
+		/// <inheritdoc/>
+		public event Action<ILifetime<TTarget>>? Finished;
+
+
+		public EmbededLifetimeStateUpdater(Action<TTarget> applyDelegate, ILifetime<TBase> baseLifetime, ILifetimeStateUpdater<TBase>? baseUpdater)
+		{
+			this.applyDelegate = applyDelegate;
+			this.baseLifetime = baseLifetime;
+			this.baseUpdater = baseUpdater;
+		}
+
+
+		/// <inheritdoc/>
+		public void Finish(ILifetime<TTarget> lifetime)
+		{
+			Finished?.Invoke(lifetime);
+		}
+
+		/// <inheritdoc/>
+		public void Update(ILifetime<TTarget> lifetime)
+		{
+			applyDelegate.Invoke(lifetime.GetBaseClone());
+			baseUpdater?.Update(baseLifetime);
+		}
+	}
+}
