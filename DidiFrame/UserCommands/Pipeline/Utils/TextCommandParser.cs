5c8c6da3833495976fb95ae93a1acb6b2591445a
LloydLion <lloydlion2211@gmail.com>
2022-07-14 22:41:40 +0300

    Text command parser behavior model

--- DidiFrame/UserCommands/Pipeline/Utils/TextCommandParser.cs
+++ DidiFrame/UserCommands/Pipeline/Utils/TextCommandParser.cs
@@ -9,7 +9,7 @@ namespace DidiFrame.UserCommands.Pipeline.Utils
 	/// </summary>
 	public class TextCommandParser : AbstractUserCommandPipelineMiddleware<string, UserCommandPreContext>
 	{
-		private readonly Options options;
+		private readonly BehaviorModel behaviorModel;
 		private readonly IUserCommandsRepository repository;
 		private readonly static Dictionary<string, UserCommandArgument.Type> regexes = new()
 		{
@@ -29,10 +29,11 @@ namespace DidiFrame.UserCommands.Pipeline.Utils
 		/// </summary>
 		/// <param name="options">Option for parser (DidiFrame.UserCommands.Pipeline.Utils.TextCommandParser.Options)</param>
 		/// <param name="repository">Command repository to detect commands</param>
-		public TextCommandParser(IOptions<Options> options, IUserCommandsRepository repository)
+		public TextCommandParser(BehaviorModel? behaviorModel, IOptions<Options> options, IUserCommandsRepository repository)
 		{
-			this.options = options.Value;
 			this.repository = repository;
+			behaviorModel = this.behaviorModel = behaviorModel ?? new BehaviorModel();
+			behaviorModel.Init(options.Value);
 		}
 
 
@@ -41,82 +42,62 @@ namespace DidiFrame.UserCommands.Pipeline.Utils
 		{
 			var server = pipelineContext.SendData.Channel.Server;
 			var cmds = repository.GetCommandsFor(server);
-			var regex = $"^[{new string(options.Prefixes.Select(s => "\\" + s).SelectMany(s => s).ToArray())}]({string.Join('|', cmds.Select(s => $"({s.Name})"))}){"{1}"}\\b";
-			var result = Regex.Matches(content, regex);
+			var command = behaviorModel.ParseCommandName(content, cmds);
 
-			if (!result.Any())
+			if (command is null)
 			{
 				pipelineContext.DropPipeline();
 				return null;
 			}
 
-			var command = cmds.Single(s => s.Name == result.Single().Groups[1].Value);
-
-			var arguments = new List<(UserCommandArgument, List<object>)>();
-			var preArgs = new List<(UserCommandArgument.Type, string)>();
+			var arguments = new Dictionary<UserCommandArgument, IReadOnlyList<object>>();
+			var selects = new List<ArgumentSelect>();
 
 			//Unless no arguemnts command
 			if (content.Length != command.Name.Length + 1)
 			{
-				var cnt = new StringBuilder(content[(command.Name.Length + 2)..]); //space include
+				var span = content.AsSpan(command.Name.Length + 1); //prefix include
 
-
-				while (cnt.Length != 0)
+				while (span.Length != 0)
 				{
-					try { preArgs.Add(parse(cnt)); }
-					catch (Exception)
+					span = span[1..];
+
+					var matches = Regex.Matches(new string(span), "^\\s+");
+					if (matches.Count == 1)
+					{
+						var slice = matches.Single().ValueSpan.Length;
+						span = span[slice..];
+					}
+					else
+					{
+						try
+						{
+							selects.Add(behaviorModel.SelectArgument(ref span));
+						}
+						catch (Exception)
+						{
+							pipelineContext.DropPipeline();
+							return null;
+						}
+					}
+				}
+
+				var selectsArray = selects.ToArray();
+				var selectsSpan = (ReadOnlySpan<ArgumentSelect>)selectsArray.AsSpan();
+
+				foreach (var argument in command.Arguments)
+				{
+					var result = behaviorModel.ProcessArgument(ref selectsSpan, argument, server);
+					if (result is null)
 					{
 						pipelineContext.DropPipeline();
 						return null;
 					}
 
-					static (UserCommandArgument.Type, string) parse(StringBuilder cnt)
-					{
-						foreach (var key in regexes.Keys)
-						{
-							var macthes = Regex.Matches(cnt.ToString(), key);
-							if (macthes.Any() == false) continue;
-							else
-							{
-								var match = macthes.Single();
-								var rawStr = match.Groups[1].Value;
-								if (cnt.Length == rawStr.Length) cnt.Clear();
-								else cnt.Remove(0, rawStr.Length + 1);
-								return (regexes[key], rawStr);
-							}
-						}
-
-						throw new InvalidOperationException("Enable to parse some command's argument");
-					}
+					arguments.Add(argument, result);
 				}
 
-				foreach (var argument in command.Arguments)
-				{
-					if (argument.IsArray)
-						arguments.Add((argument, preArgs.Select(s => ConvertArgument(s.Item2, s.Item1, server)).ToList()));
-					else
-					{
-						//order is important (thanks LINQ)
-						var pa = preArgs.Take(argument.OriginTypes.Count).ToArray();
-
-						if (pa.Length != argument.OriginTypes.Count)
-						{
-							pipelineContext.DropPipeline();
-							return null;
-						}
-						for (int i = 0; i < pa.Length; i++)
-							if(argument.OriginTypes[i] != pa[i].Item1)
-							{
-								pipelineContext.DropPipeline();
-								return null;
-							}
-
-						arguments.Add((argument, pa.Select(s => ConvertArgument(s.Item2, s.Item1, server)).ToList()));
-						preArgs.RemoveRange(0, argument.OriginTypes.Count);
-					}
-				}
-
-				return new(pipelineContext.SendData.Invoker, pipelineContext.SendData.Channel, command, arguments.ToDictionary(s => s.Item1, s => (IReadOnlyList<object>)s.Item2));
+				return new(pipelineContext.SendData.Invoker, pipelineContext.SendData.Channel, command, arguments);
 			}
 			else
 			{
@@ -132,29 +113,6 @@ namespace DidiFrame.UserCommands.Pipeline.Utils
 			}
 		}
 
-		private object ConvertArgument(string rawStr, UserCommandArgument.Type ttype, IServer server)
-		{
-			switch (ttype)
-			{
-				case UserCommandArgument.Type.Integer: return int.Parse(rawStr);
-				case UserCommandArgument.Type.Double: return double.Parse(rawStr, new CultureInfo("en-US"));
-				case UserCommandArgument.Type.String: return rawStr.StartsWith('\"') && rawStr.EndsWith('\"') ? rawStr[1..^1] : rawStr;
-				case UserCommandArgument.Type.Member:
-					var match = Regex.Match(rawStr, @"^<@!?(\d+)>$").Groups[1].Value;
-					var id = ulong.Parse(match);
-					return server.GetMember(id); //NOTE mention struct: <@USERID> or <@!USERID>
-				case UserCommandArgument.Type.Role: return server.GetRole(ulong.Parse(rawStr[2..^1])); //NOTE mention struct: <@&ROLEID>
-				case UserCommandArgument.Type.Mentionable:
-					if (Regex.IsMatch(rawStr, @"^<@!?(\d+)>$")) return ConvertArgument(rawStr, UserCommandArgument.Type.Member, server);
-					else return ConvertArgument(rawStr, UserCommandArgument.Type.Role, server);
-				case UserCommandArgument.Type.TimeSpan: return TimeSpan.Parse(rawStr);
-				case UserCommandArgument.Type.DateTime:
-					var split = rawStr.Split('|');
-					return new DateTime(DateTime.Parse(split[0] + '.' + DateTime.Now.Year + ' ' + split[1]).Ticks, DateTimeKind.Local);
-				default: throw new Exception();
-			}
-		}
-
 
 		/// <summary>
 		/// Options for DidiFrame.UserCommands.Pipeline.Utils.TextCommandParser
@@ -165,6 +123,116 @@ namespace DidiFrame.UserCommands.Pipeline.Utils
 			/// Char array of prefixes by that command can start
 			/// </summary>
 			public string Prefixes { get; set; } = "";
+		}
+
+		public struct ArgumentSelect
+		{
+			public ArgumentSelect(string selectedString, UserCommandArgument.Type type)
+			{
+				SelectedString = selectedString;
+				Type = type;
+			}
+
+
+			public string SelectedString { get; }
+
+			public UserCommandArgument.Type Type { get; }
+		}
+
+		public class BehaviorModel
+		{
+			private Options? options;
+
+
+			public Options Options => options ?? throw new NullReferenceException();
+
+
+			public void Init(Options options)
+			{
+				this.options = options;
+			}
+
+			public virtual UserCommandInfo? ParseCommandName(string rawInput, IUserCommandsCollection commands)
+			{
+				var regex = $"^[{new string(Options.Prefixes.SelectMany(s => "\\" + s).ToArray())}]({string.Join('|', commands.Select(s => $"({s.Name})"))}){"{1}"}\\b";
+				var result = Regex.Matches(rawInput, regex);
+				if (!result.Any()) return null;
+				else return commands.GetCommad(result.Single().Groups[1].Value);
+			}
+
+			protected virtual object ConvertArgumentValue(string rawStr, UserCommandArgument.Type ttype, IServer server)
+			{
+				switch (ttype)
+				{
+					case UserCommandArgument.Type.Integer: return int.Parse(rawStr);
+					case UserCommandArgument.Type.Double: return double.Parse(rawStr, new CultureInfo("en-US"));
+					case UserCommandArgument.Type.String: return rawStr.StartsWith('\"') && rawStr.EndsWith('\"') ? rawStr[1..^1] : rawStr;
+					case UserCommandArgument.Type.Member:
+						var match = Regex.Match(rawStr, @"^<@!?(\d+)>$").Groups[1].Value;
+						var id = ulong.Parse(match);
+						return server.GetMember(id); //NOTE mention struct: <@USERID> or <@!USERID>
+					case UserCommandArgument.Type.Role: return server.GetRole(ulong.Parse(rawStr[2..^1])); //NOTE mention struct: <@&ROLEID>
+					case UserCommandArgument.Type.Mentionable:
+						if (Regex.IsMatch(rawStr, @"^<@!?(\d+)>$")) return ConvertArgumentValue(rawStr, UserCommandArgument.Type.Member, server);
+						else return ConvertArgumentValue(rawStr, UserCommandArgument.Type.Role, server);
+					case UserCommandArgument.Type.TimeSpan: return TimeSpan.Parse(rawStr);
+					case UserCommandArgument.Type.DateTime:
+						var split = rawStr.Split('|');
+						return new DateTime(DateTime.Parse(split[0] + '.' + DateTime.Now.Year + ' ' + split[1]).Ticks, DateTimeKind.Local);
+					default:
+						throw new ImpossibleVariantException();
+				}
+			}
+
+			public virtual ArgumentSelect SelectArgument(ref ReadOnlySpan<char> rawString)
+			{
+				foreach (var key in regexes.Keys)
+				{
+					var macthes = Regex.Matches(new string(rawString), key);
+					if (macthes.Any() == false) continue;
+					else
+					{
+						var match = macthes.Single();
+						var rawStr = match.Groups[1].Value;
+						rawString = rawString[..rawStr.Length];
+						return new(rawStr, regexes[key]);
+					}
+				}
+
+				throw new InvalidOperationException("Enable to select some command's argument");
+			}
+
+			public virtual IReadOnlyList<object>? ProcessArgument(ref ReadOnlySpan<ArgumentSelect> selects, UserCommandArgument argument, IServer server)
+			{
+				var result = new List<object>();
+
+				if (argument.IsArray)
+				{
+					foreach (var item in selects)
+						result.Add(ConvertArgumentValue(item.SelectedString, item.Type, server));
+					selects = ReadOnlySpan<ArgumentSelect>.Empty;
+				}
+				else
+				{
+					var pa = selects[..argument.OriginTypes.Count];
+
+					if (pa.Length != argument.OriginTypes.Count)
+					{
+						return null;
+					}
+					for (int i = 0; i < pa.Length; i++)
+						if (argument.OriginTypes[i] != pa[i].Type)
+						{
+							return null;
+						}
+
+					foreach (var item in pa)
+						result.Add(ConvertArgumentValue(item.SelectedString, item.Type, server));
+					selects = selects[argument.OriginTypes.Count..];
+				}
+
+				return result;
+			}
 		}
 	}
 }
