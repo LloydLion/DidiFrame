50db787dad6764b3b3085807452052787e374991
LloydLion <lloydlion2211@gmail.com>
2023-06-09 01:45:06 +0300

    Clear TestProject

--- TestProject/GlobalUsings.cs
+++ TestProject/GlobalUsings.cs
@@ -1,6 +1,3 @@
 ﻿global using NUnit.Framework;
 global using DidiFrame.Clients;
 global using DidiFrame.Entities;
-global using DidiFrame.Testing.Client;
-global using DidiFrame.Testing.Data;
-global using DidiFrame.Testing.UserCommands;
